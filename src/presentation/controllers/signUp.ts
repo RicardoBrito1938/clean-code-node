df4c37ac3e8f8ab233952b1ab514d17268fa7d8c
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2022-08-31 21:44:58 +0100

    refactor: move error to a single import

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,9 +1,8 @@
 import { EmailValidator } from "./../protocols/email-validator";
 import { Controller } from "./../protocols/controller";
 import { badRequest, serverError } from "./../helpers/http-helper";
-import { MissingParamError } from "../errors/missing-param-errors";
 import { HttpRequest, HttpResponse } from "./../protocols/http";
-import { InvalidParamError } from "../errors/invalid-param-errors.";
+import { InvalidParamError, MissingParamError } from "../errors";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
-import { MissingParamError } from "../errors/missing-param-errors";
-import { InvalidParamError } from "../errors/invalid-param-errors.";
 import { SignUpController } from "./signUp";
 import { EmailValidator } from "../protocols/email-validator";
-import { ServerError } from "../errors/server-error";
+import { InvalidParamError, MissingParamError, ServerError } from "../errors";
 
 interface SutTypes {
   sut: SignUpController;
